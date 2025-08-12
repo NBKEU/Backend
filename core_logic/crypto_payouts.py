3085dbcd72bf49ef9c923d20533bb711beebcbad
NBKEU <nbkgroupuk@gmail.com>
2025-08-12 11:50:30 +0530

    Update crypto_payouts.py

--- core_logic/crypto_payouts.py
+++ core_logic/crypto_payouts.py
@@ -72,10 +72,7 @@ def _create_erc20_transaction(to_address, amount_in_wei):
     }
 
     # Sign the transaction with your private key
-       return signed_txn.raw_transaction.hex()
-    
-    # Return the raw, signed transaction hex string
-    return signed_txn.rawTransaction.hex()
+     return signed_txn.raw_transaction.hex()
 def payout_erc20(to_address, amount):
     """
     Initiates an ERC-20 crypto payout via Infura or Alchemy.
